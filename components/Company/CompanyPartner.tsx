8ed5e0488228f81040c789fdbd183d1eacececf1
MKSBRYAN <officielsamuelmok63@>
2024-12-06 22:01:27 -0800

    Commit the version 2 of urisat site

--- components/Company/CompanyPartner.tsx
+++ components/Company/CompanyPartner.tsx
@@ -6,7 +6,7 @@ const CompanyPartner = () => {
     <div className='pt-[6rem] pb-[3rem]'>
         <h1 className='mb-[1.5rem] font-bold text-[22px] text-center md:text-[30px] text-black-1'> Company partner also  worked with us</h1>
         <p className='w-[90%] sm:w-[85%] md:w-[75%] lg:w-[65%] mx-auto text-[17px] text-center text-gray-950 opacity-90'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore dignissimos tempora ipsa repellat non, veritatis iste optio necessitatibus natus dolores ex quaerat ipsam eaque. Necessitatibus magni sit officiis minima. Id?
+        we firmly believe that strategic partnerships are the key to innovating and progressing in the digital world. We are proud to collaborate with industry leaders, cutting-edge technology companies and passionate innovators to deliver exceptional solutions for our customers.
         </p>
         <p className='w-[90%] mt-[1.7rem] cursor-pointer font-[500] sm:w-[85%] md:w-[75%] lg:w-[65%] mx-auto text-[17px] text-center text-gray-950 opacity-90'>
             Explore Detail
