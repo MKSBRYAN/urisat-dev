96db53667a3f20591df9324025b3f61b7728d26b
MKSBRYAN <officielsamuelmok63@>
2024-12-10 11:16:20 -0800

    Commit the testimonial 10 Dec

--- components/Home.tsx
+++ components/Home.tsx
@@ -5,22 +5,15 @@ import CustomerSupport from './CustomerSuport/CustomerSupport'
 import Price from './Price/Price'
 import SupportTeam from './SupportTeam/SupportTeam'
 import CompanyPartner from './Company/CompanyPartner'
-import Technology from './Technology/Technology'
-import Particles from 'react-tsparticles'
-import ParticlesBackground from './ParticlesBackground'
+// import Technology from './Technology/Technology'
+
 import ScrollToTop from './ScrollToTop/ScrollToTop'
-import AnimatedSection from './Common/AnimatedSection'
 import { AnimatedTestimonialsDemo } from './Testimonial/Testimonia'
 const Home = () => {
   return (
     <div className='bg-transparent'>
       {/* <ParticlesBackground/> */}
-     
-
       <Hero/>
-      
-     
-     
       <Features/>
       <CustomerSupport/>
       <Price/>
